81c50c2e20581affd05fe8b5308486667653e89f
Jake Crawford <jake.crawford@outlook.com>
2014-12-30 20:27:39 -0700

    Add validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+	before_save {
+		self.email = email.downcase
+		self.first = first.capitalize
+		self.last = last.capitalize
+	}
 
+	validates :email, presence: true
+	validates_format_of :email, :with => /@/
+	validates :first, presence: true
+	validates :last, presence: true
+	
 	has_secure_password
 end
